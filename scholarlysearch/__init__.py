2238e182eb18f318b108246dae2b77c876edabbf
William Terry <dolphin85204@me.com>
2024-01-22 19:19:30 -0700

    Added security rules, basic menu, basic elsevier rest class

---
+++ scholarlysearch/__init__.py
@@ -0,0 +1,13 @@
+import os
+import configparser
+
+config = configparser.ConfigParser()
+
+config.read(os.path.join(os.path.dirname(__file__), 'application.properties'))
+
+for (key, value) in config.items('variables'):
+    os.environ[key] = value
+    
+    
+# Testing, remove this
+print("ApiKey: " + os.environ.get('elsevier-api-key'))
