d9a6888c0f428264d6bbcc767a3b6b101120f0b3
William Terry <dolphin85204@me.com>
2024-01-30 20:00:55 -0700

    Minor name changes to more pythonic format

--- scholarlysearch/__init__.py
+++ scholarlysearch/__init__.py
@@ -3,7 +3,7 @@ import configparser
 
 config = configparser.ConfigParser()
 
-config.read(os.path.join(os.path.dirname(__file__), 'application.properties'))
+config.read(os.path.join(os.path.dirname(__file__), '.env'))
 
 for (key, value) in config.items('variables'):
     os.environ[key] = value
