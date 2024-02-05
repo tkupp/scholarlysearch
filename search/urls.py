f648df11d273608bf4fa69927369f2d2c09be28d
William Terry <dolphin85204@me.com>
2024-02-05 08:19:24 -0700

    further work on the front end, and on wiring up for REST/swagger

--- search/urls.py
+++ search/urls.py
@@ -1,7 +1,13 @@
 from django.urls import re_path
+from django.urls import path
+from rest_framework_swagger.views import get_swagger_view
+
+schema_view = get_swagger_view(title='Scholarly Search API')
 
 from . import views
 
 urlpatterns = [
     re_path(r'^$', views.index, name='main'),
+    path("searchElsevier", views.searchElsevier, name='searchElsevier'),
+    path("swagger", schema_view)
 ]
