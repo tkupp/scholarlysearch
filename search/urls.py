ed4befdb25fa99b0b4ac6b1a155265ecb81a6dea
William Terry <dolphin85204@me.com>
2024-02-07 17:48:17 -0700

    added very basic REST framework

--- search/urls.py
+++ search/urls.py
@@ -1,13 +1,10 @@
 from django.urls import re_path
 from django.urls import path
-from rest_framework_swagger.views import get_swagger_view
-
-schema_view = get_swagger_view(title='Scholarly Search API')
 
 from . import views
 
 urlpatterns = [
     re_path(r'^$', views.index, name='main'),
     path("searchElsevier", views.searchElsevier, name='searchElsevier'),
-    path("swagger", schema_view)
+    path("api/elsevier", views.apiSearchElsevier, name='apiSearchElsevier'),
 ]
