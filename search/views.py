6db5c347269d3b653ced9c7b5fb8b6e6ae652aed
William Terry <dolphin85204@me.com>
2024-02-23 16:09:59 -0700

    work on swagger

--- search/views.py
+++ search/views.py
@@ -3,9 +3,9 @@ from django.views.generic import TemplateView
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from drf_yasg.utils import swagger_auto_schema
-from drf_yasg import openapi
 from rest_framework import status
+from drf_spectacular.utils import extend_schema, OpenApiParameter, OpenApiExample
+from drf_spectacular.types import OpenApiTypes
 
 from .elsevier_rest_class import Elsevier_rest
 from .arxiv_rest_class import Arxiv_rest
@@ -29,6 +29,59 @@ def search_elsevier(request):
     return render(request, 'search/search_elsevier.html', context)
     
 
+@extend_schema( 
+        parameters=[
+            OpenApiParameter( 
+                name='q', 
+                type={'type': 'string'}, 
+                description="The search string",
+                location=OpenApiParameter.QUERY, 
+                required=True, 
+            ),
+            OpenApiParameter( 
+                name='num_pages', 
+                type={'type': 'integer'}, 
+                description="The number of results to return.",
+                location=OpenApiParameter.QUERY, 
+                required=False, 
+            ),
+            OpenApiParameter( 
+                name='start_page', 
+                type={'type': 'integer'}, 
+                description="Where you want the results to start from.",
+                location=OpenApiParameter.QUERY, 
+                required=False, 
+            ),
+        ], 
+        responses={
+            200: OpenApiTypes.OBJECT
+        },
+        examples=[
+            OpenApiExample(
+                name="Example Elsevier Search",
+                summary="The results for an Elsevier search",
+                description="Example Elsevier Search",
+                value={
+                    "status": 200,
+                    "results": 16128,
+                    "currentPage": 0,
+                    "pageSize": 1,
+                    "resultRows": [
+                        {
+                            "identifier": "DOI:10.1016/j.infoandorg.2024.100503",
+                            "url": "https://api.elsevier.com/content/article/pii/S1471772724000034",
+                            "title": "Generative mechanisms of AI implementation: A critical realist perspective on predictive maintenance",
+                            "creator": "Alexander Stohr",
+                            "publication": "Information and Organization",
+                            "loadDate": "24-02-21" 
+                        },
+                    ]
+                    
+                },
+            response_only=True
+            )
+        ],
+    ) 
 @api_view(['GET'])
 def api_search_elsevier(request):
     if request.method == 'GET':
